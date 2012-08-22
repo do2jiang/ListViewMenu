638dc626e970b948028b62cfc2bdccec4d76c9f7
elijahsh <elijah.sh@gmail.com>
2012-08-22 16:23:14 +0400

    getView ViewHolder optimization.

--- src/com/elijahsh/view/ListViewMenuAdapter.java
+++ src/com/elijahsh/view/ListViewMenuAdapter.java
@@ -20,28 +20,45 @@ import java.util.List;
 public class ListViewMenuAdapter extends ArrayAdapter<ListViewMenuItem> {
     private Context mContext;
     private int mLayoutResourceId;
+    private LayoutInflater mInflater;
 
     public ListViewMenuAdapter(Context context, List<ListViewMenuItem> objects) {
         super(context, 0, objects);
         mContext = context;
         mLayoutResourceId = R.layout.view_navigation_row;
+        mInflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    static private class ViewHolder {
+        public ImageView icon;
+        public TextView title;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return 0;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View rowView = convertView;
+        ViewHolder viewHolder;
         if (rowView == null) {
-            LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            rowView = inflater.inflate(mLayoutResourceId, parent, false);
+            rowView = mInflater.inflate(mLayoutResourceId, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.icon = (ImageView) rowView.findViewById(R.id.navigation_icon);
+            viewHolder.title = (TextView) rowView.findViewById(R.id.navigation_title);
         } else {
-
+            viewHolder = (ViewHolder)rowView.getTag();
         }
 
-        ImageView icon = (ImageView) rowView.findViewById(R.id.navigation_icon);
-        TextView title = (TextView) rowView.findViewById(R.id.navigation_title);
-
-        icon.setImageDrawable(getItem(position).getIcon());
-        title.setText(getItem(position).getTitle());
+        viewHolder.icon.setImageDrawable(getItem(position).getIcon());
+        viewHolder.title.setText(getItem(position).getTitle());
         return rowView;
     }
 }
